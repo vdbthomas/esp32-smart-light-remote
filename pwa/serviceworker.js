2c39d12a8b3f82d0d0686ccbddf098724d0e4d0b
vdbthomas <63299095+vdbthomas@users.noreply.github.com>
2022-03-02 15:32:41 +0100

    Add project files

---
+++ pwa/serviceworker.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e => {                                                                     // when service worker is installed
+    e.waitUntil(                                                                                            
+        caches.open("slr-static").then(cache => {                                                           // create new cache and add the following:
+            return cache.addAll(["./", "./src/script.js", "./images/vdb-vdbsoftware-logo-192x192.png"])
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {                                                                       // whenever the browser is trying to fetch (load) the website
+    console.log(`Intercepting fetch request for: ${e.request.url}`);
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch(e.request);                                                            // return from cache before fetching from the webserver --> this will also make sure website is available if there is no connection to the webserver
+        })
+    );
+});
