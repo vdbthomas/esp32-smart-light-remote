ae687061cde7f0e1d7ad5642d525ad37df35590d
vdbthomas <63299095+vdbthomas@users.noreply.github.com>
2022-03-09 12:13:00 +0100

    Update update.js

--- pwa/src/update.js
+++ pwa/src/update.js
@@ -1,1 +1,15 @@
+var request = new XMLHttpRequest();
+request.open("GET", "https://vdbthomas.github.io/esp32-smart-light-remote/pwa/manifest.json", false);
+request.send(null)
+var manifest = JSON.parse(request.responseText);
 
+if (localStorage.localVersion) {
+    if(localStorage.localVersion !== manifest.version) {
+        caches.open(slr-static).then(function(cache) {
+            cache.addAll(["./app.html", "./src/setup.js", "./src/install.js", "./images/vdb-vdbsoftware-logo-192x192.png"])
+          });          
+        localStorage.localVersion = manifest.version;
+    }
+} else {
+    localStorage.localVersion = manifest.version;
+}
