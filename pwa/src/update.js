bc36892ff57a82a923e43cb194133778363dbc13
vdbthomas <63299095+vdbthomas@users.noreply.github.com>
2022-03-09 17:51:06 +0100

    Update update.js

--- pwa/src/update.js
+++ pwa/src/update.js
@@ -5,13 +5,12 @@ var manifest = JSON.parse(request.responseText);
 
 if (localStorage.localVersion) {
     if(localStorage.localVersion !== manifest.version) {
-        caches.open('slr-static').then(function(cache) {
-            cache.delete("slr-static").then(function(response) {
+            caches.open("slr-static").then(cache => {                                                       
+                return cache.addAll(["./app.html", "./src/setup.js", "./src/install.js", "./images/vdb-vdbsoftware-logo-192x192.png"]);
                 console.log("New version detected, cache deleted");
                 localStorage.localVersion = manifest.version;
                 // location.reload();
             });
-        });
     }
 } else {
     localStorage.localVersion = manifest.version;
