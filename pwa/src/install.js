0755d99c87c78ef79411024d0d4ce75e8d6f5338
vdbthomas <63299095+vdbthomas@users.noreply.github.com>
2022-04-03 19:08:07 +0200

    Update install.js

--- pwa/src/install.js
+++ pwa/src/install.js
@@ -1,35 +1,26 @@
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent the mini-infobar from appearing on mobile.
-    event.preventDefault();
-    console.log('beforeinstallprompt', event);
-    // Stash the event so it can be triggered later.
+    event.preventDefault();     // Prevent the mini-infobar from appearing on mobile
+    console.log('beforeinstallprompt', event);      // Stash event
     window.deferredPrompt = event;
-    // installButton.classList.toggle('hidden', false); - show download button if installable; 
-    // notInstallable.classList.toggle('hidden', true); -  hide "not installable" 
+    // installButton.classList.toggle('hidden', false); - show download button if installable; (for development purposes always visible)
+    // notInstallable.classList.toggle('hidden', true); -  hide "not installable" (non-existent yet)
   });
 
   async function installPWA() {
-    console.log('butInstall-clicked');
+    console.log('installButton');
     const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-      // The deferred prompt isn't available.
+    if (!promptEvent) {     // The deferred prompt isn't available.
       return;
     }
-    // Show the install prompt.
-    promptEvent.prompt();
-    // Log the result
+    promptEvent.prompt();       // Show the install prompt.
     const result = await promptEvent.userChoice;
-    console.log('userChoice', result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
-    window.deferredPrompt = null;
-    // Hide the install button.
-    divInstall.classList.toggle('hidden', true);
+    console.log('userChoice', result);      // Log the result
+    window.deferredPrompt = null;       // Reset the deferred prompt variable, since prompt() can only be called once
+    // installButton.classList.toggle('hidden', true); - hide install button (for development purposes always visible)
   };
 
   window.addEventListener('appinstalled', (event) => {
     console.log('appinstalled', event);
-    // Clear the deferredPrompt so it can be garbage collected
-    window.deferredPrompt = null;
-    window.location.replace("/esp32-smart-light-remote/pwa/installcomplete.html");                                                     // set location to main app page (otherwise it would still be at the original download page when launched on desktop)
+    window.deferredPrompt = null;   // Clear the deferredPrompt
+    window.location.replace("/esp32-smart-light-remote/pwa/installcomplete.html");      // set location to main app page (otherwise it would still be at the original download page when launched on desktop)
   });
